51c7b02eb944d62a3ef47c825f1fa68e42b8f6a0
mohammedterfa <mohammedterfa@gmail.com>
2023-10-02 12:41:54 +0300

    install breeze and make admin controller and category controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\SignUpRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
